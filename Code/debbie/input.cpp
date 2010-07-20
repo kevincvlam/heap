2ddba3c67376d58467209eceb14fd315ed5b75d1
tachyonic <kfc.feather@gmail.com>
2010-07-20 18:59:40 -0400

    editted to store info while reading in file

--- Code/debbie/input.cpp
+++ Code/debbie/input.cpp
@@ -6,34 +6,24 @@ Input::Input(){
 
 void Input::store_info(const char * file){
 
-    // PART 1 extract data from file
     ifstream in (file);
     string s; // holds each line
-    vector <string> v; // holds document
     while (getline(in,s)){
-        v.push_back(s);
-        s.clear();
-    }
-    in.close();
-    
-
-    //PART 2 view v index by index and change information matrix based on findings
-    int i;
-    for(i=0;i<(unsigned)v.size();i++){
-          //cout << "viewing: " << v[i] << endl;
-        if (v[i].rfind("First name",v[i].size()-1)!=string::npos){
+        if (s.rfind("First name",s.size()-1)!=string::npos){
             Student *TA = new Student;
-            TA->firstName = v[i].substr(12,v[i].size()-1);
+            TA->firstName = s.substr(12,s.size()-1);
             info.push_back(TA);
             cout << "added first name: " << info[numTA]->firstName << endl;
             numTA++;
-        } else if (v[i].rfind("Middle name",v[i].size()-1)!=string::npos){
-            info[numTA-1]->middleName = v[i].substr(13,v[i].size()-1);
+        } else if (s.rfind("Middle name",s.size()-1)!=string::npos){
+            info[numTA-1]->middleName = s.substr(13,s.size()-1);
             cout << "added " << info[numTA-1]->firstName << "'s middle name: " << info[numTA-1]->middleName << endl;
-        } else if (v[i].rfind("Last name",v[i].size()-1)!=string::npos){
-            info[numTA-1]->lastName = v[i].substr(11,v[i].size()-1);
+        } else if (s.rfind("Last name",s.size()-1)!=string::npos){
+            info[numTA-1]->lastName = s.substr(11,s.size()-1);
             cout << "added last name: " << info[numTA-1]->lastName << endl;
         }
+        s.clear();
     }
+    in.close();
     
 }
