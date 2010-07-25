a510289850390e440ed3c9cebbb49c1bfb9e6956
Kevin Lam <i.am.kev@gmail.com>
2010-07-24 21:20:45 -0400

    pulling master

--- Code/kevin/rfind.cpp
+++ Code/kevin/rfind.cpp
@@ -5,10 +5,14 @@ using namespace std;
 
 int main ()
 {
-  string str ("Does rfind find CSC108H?");
-  string key ("CSC108H");
+  string str ("Does rfind find CSC108H? 1st");
+  string key ("1st");
   size_t found;
 
+  found=str.rfind(key);
+  if (found!=string::npos)
+    str.replace (found,key.length(),"Yes!");
+  key = "CSC108H";
   found=str.rfind(key);
   if (found!=string::npos)
     str.replace (found,key.length(),"CSC Courses");
