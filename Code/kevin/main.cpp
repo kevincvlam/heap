1f8b231059d7f5428e5a8ea1feaffb361564e37e
Kevin Lam <i.am.kev@gmail.com>
2010-08-04 15:36:22 -0400

    working on solving function

--- Code/kevin/main.cpp
+++ Code/kevin/main.cpp
@@ -3,38 +3,76 @@
 #include "sorting.h"
 #include "score.h"
 
+int mostConstrainedCourse(vector <Course*> courseinfo)
+{
+    unsigned int k;
+    int min = 999999999;
+    int minid = 0;
+    for(k=0; k< courseinfo.size(); k++)
+    {
+        if(courseinfo[k]->freedom < min){
+        min = courseinfo[k]->freedom;
+        minid = courseinfo[k]->id;}
+    }
+    return minid;
+}
 
+vector <Student*> removeCandidate(vector <Student*> candidates, unsigned int id)
+{
+    unsigned int k;
+    for(k = 0; k< candidates.size(); k++)
+    {
+        if(id == candidates[k]->id)
+        {
+            candidates.erase(candidates.begin()+k);
+        }
+    }
+    return candidates;
+}
 
-int assign(vector <Student*> TAinfo, vector <Course*> courseinfo, vector <Student*> assignment[NUM_COURSES], int gHours)
+int assign(vector <Student*> TAinfo, vector <Course*> courseinfo, vector <Student*> candidates[NUM_COURSES], vector <Student*> assignment[NUM_COURSES], int gHours)
 {
     //Function Objects
     Input in;
     Sorting sort;
     Score score;
 
-    if(score.assignment(TAinfo, courseinfo, assignment, gHours) < 500) {return 0;}
-
-    else{return 1;}
-
     //check all possibilities
-
     //make an assignment
     //assign TA to most constrained course
-
-
     //remove TA from set of possibilities in that course
+    unsigned int i;
+    unsigned int id = mostConstrainedCourse(courseinfo);
 
-//    if(solve(assignment)
-//    {
-//        printAssignment
-//        return 1;
-//    }
+    //if no candidates break;
+    if(candidates[id].size() == 0)
+    { return 0; }
+
+    //if score is > 500 return 1;
+    else if(score.assignment(TAinfo, courseinfo, assignment, gHours) > 500) {return 1;}
+
+    for(i=0; i< candidates[id].size(); i++)
+    {
+        assignment[id].push_back(TAinfo[i]);
+        candidates[id] = removeCandidate(candidates[id], TAinfo[i]->id);
+        courseinfo[id]->TAships--;
+        courseinfo = in.updateConstraintInfo(courseinfo, TAinfo);
+        TAinfo[i]->TAhoursOwed -= 54;
+
+        if(assign(TAinfo, courseinfo, candidates, assignment, gHours))
+        {
+        sort.printAssignment(assignment, 0);
+        return 1;
+        }
+    }
+
+
     return 0;
 }
 
 //1. Takes the name of a file to view as the first command-line argument
 int main(int argc, char* argv[]) {
-    if (argc < 3) { cout << "Error: Use 1 Argument." << endl; return EXIT_FAILURE;}
+    if (argc < 3) { cout << "Error: Use 2 Argument." << endl; return EXIT_FAILURE;}
     /** Initialization of Objects and Storing of Information **/
 
     //Function Objects
@@ -66,33 +104,33 @@ int main(int argc, char* argv[]) {
 
 
    /** Prints out the Course Data **/
-    unsigned int i;
-    for(i =0; i< courseinfo.size(); i++)
-    {
-        cout << courseinfo[i]->id << ". " << courseinfo[i]->code << endl;
-        cout << "Enrolled: " << courseinfo[i]->enrolled << " TAships: " << courseinfo[i]->TAships << endl;
-        cout << "Freedom: " << courseinfo[i]->freedom << endl;
-        cout << "numPref1st: " << courseinfo[i]->numPref1st << endl;
-        cout << "Level: " << courseinfo[i]->level << endl;
-    }
+//    unsigned int i;
+//    for(i =0; i< courseinfo.size(); i++)
+//    {
+//        cout << courseinfo[i]->id << ". " << courseinfo[i]->code << endl;
+//        cout << "Enrolled: " << courseinfo[i]->enrolled << " TAships: " << courseinfo[i]->TAships << endl;
+//        cout << "Freedom: " << courseinfo[i]->freedom << endl;
+//        cout << "numPref1st: " << courseinfo[i]->numPref1st << endl;
+//        cout << "Level: " << courseinfo[i]->level << endl;
+//    }
 
   /** Make a Dummy Assignment **/
-//  unsigned int j;
-//  unsigned int q;
-//  unsigned int k = 0;
-//  for(j =0; j < courseinfo.size(); j++)
-//  {
-//      for(q = 0; q <5; q++)
-//      {
-//          assignment[j].push_back(TAinfo[k++]);
-//      }
-//  }
-//  sort.printAssignment(assignment, 0);
-//  cout << "The fitness of this assignment is: " << score.assignment(TAinfo, courseinfo, assignment, gHours) << endl;
+  unsigned int j;
+  unsigned int q;
+  unsigned int k = 0;
+  for(j =0; j < courseinfo.size(); j++)
+  {
+      for(q = 0; q <6; q++)
+      {
+          assignment[j].push_back(TAinfo[k++]);
+      }
+  }
+  assignment[0] = removeCandidate(assignment[0], TAinfo[0]->id);
+  sort.printAssignment(assignment, 0);
+  cout << "The fitness of this assignment is: " << score.assignment(TAinfo, courseinfo, assignment, gHours) << endl;
 
 
-
-
+ //   int solve = assign(TAinfo, courseinfo, candidates, assignment, gHours);
 
     return 0;
 }
